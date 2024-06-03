2ecd694d0544596744933a68f93b8b09ab6b2372
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-03 16:34:10 -0400

    Create messages model

---
+++ models/messages.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+   title: { type: String, required: true },
+   timestamp: { type: Date, required: true, default: Date.now },
+   text: { type: String, required: true },
+   author: { type: Schema.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose.Model("Message", MessageSchema);
