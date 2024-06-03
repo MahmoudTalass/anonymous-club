aa516ed18ab563c0f821da8b437e822805cf5bb1
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-03 16:35:03 -0400

    Create initial DB and express-session setup

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
 
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+const MongoStore = require("connect-mongo");
+
+// dotenv config
+require("dotenv").config();
+
+// Database setup
+const mongoose = require("mongoose");
+main().catch((err) => console.log(err));
+async function main() {
+   await mongoose.connect(process.env.MONGODB_URL);
+}
+
+// Routes
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -13,14 +27,27 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+   session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+         maxAge: 14 * 24 * 60 * 60,
+      },
+      store: MongoStore.create({
+         mongoUrl: process.env.MONGODB_URL,
+         collectionName: "sessions",
+      }),
+   })
+);
+app.use(passport.session());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
