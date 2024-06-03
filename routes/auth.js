b7591458e17cb1cc085a4a596f5d1d946e6eed5f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-03 17:28:51 -0400

    Create authentication routes

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/login");
+
+router.post("/login");
+
+router.get("/register");
+
+router.post("/register");
+
+router.post("/logout");
+
+module.exports = router;
