1e064670e7b5c692d0b1b6da98706fba84fae378
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-03 16:27:25 -0400

    Create user model

---
+++ models/users.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+   firstname: { type: String, required: true },
+   lastname: { type: String, required: true },
+   email: { type: String, required: true, lowercase: true },
+   password: { type: String, required: true },
+   membership_status: { type: Boolean, required: true },
+});
+
+UserSchema.virtual("fullname").get(function () {
+   return this.firstname + " " + this.lastname;
+});
+
+module.exports = mongoose.Model("User", UserSchema);
